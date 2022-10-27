8746db81071eb45e39a43a6a600fce2e4ca527ac
faisal19981 <ahmadfaisalstyh@gmail.com>
2022-10-27 15:02:24 +0700

    soal 3

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -8,7 +8,17 @@
   </head>
   <body>
       <h1 class="text-center mt-5">{{ $text }}</h1>
-      
+      <div class="container">
+          <hr>
+      </div>
+      <br>
+      <div class="container mt-2 text-center">
+        <a href="{{ URL::to('mahasiswa/pendaftaran') }}" type="button" class="btn btn-primary">Pendaftaran</a>
+        
+        <a href="{{ URL::to('mahasiswa/ujian') }}" type="button" class="btn btn-primary">Ujian</a>
+        
+        <a href="{{ URL::to('mahasiswa/nilai') }}" type="button" class="btn btn-primary">Nilai</a>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
